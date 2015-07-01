f897999e6ba99aa94b1a172991da453a693fd074
Bradford Smith <bradford.smith94@gmail.com>
2015-07-01 10:30:12 -0400

    Install.sh progress
    Removed basic function and added makeSymLinks function, restored
    original functionality when no flags are passed. This sets the stage for
    using the new variables controlled with flags to add new functionality.

--- install.sh
+++ install.sh
@@ -1,96 +1,26 @@
 #!/bin/bash
 ################################################################################
+# Bradford Smith
+# install.sh
+# updated: 7/1/2015
+#
 # This script can be run to install my dotfiles.
 #
 # Inspired by: github.com/michaeljsmalley/dotfiles/makesymlinks.sh
 #
 ################################################################################
 
-########## Functions ###########################################################
-
-##### Basic ####################################################################
-function basic
-{
-    # create dotfiles_old in homedir
-    echo "Creating $olddir for backup of any existing dotfiles in $HOME"
-    mkdir -p $olddir
-
-    # create config_old in homedir
-    echo "Creating $oldconfig for backup of any existing files in $HOME/.config"
-    mkdir -p $oldconfig
-
-    # change to the dotfiles directory
-    echo ""
-    echo "#########################################"
-    echo "Changing to the $dir directory ..."
-    cd $dir
-
-    # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the $HOME/dotfiles directory specified in $files
-    echo "Moving any existing dotfiles from $HOME to $olddir"
-    for file in $files; do
-        if [ "$(readlink $HOME/.$file)" = $dir/$file ]
-        then
-            echo "$file is already linked here, skipping..."
-        else
-            mv $HOME/.$file $olddir/
-            echo "Creating symlink to $file in home directory."
-            ln -s $dir/$file $HOME/.$file
-        fi
-    done
-
-    # change to config directory
-    echo ""
-    echo "#########################################"
-    echo "Changing to the $config directory ..."
-    cd $config
-
-    # move existing and make symlinks
-    echo "Moving any existing files from $HOME/.config to $oldconfig"
-    for file in $config_files; do
-        if [ "$(readlink $HOME/.config/$file)" = $config/$file ]
-        then
-            echo "$file is already linked here, skipping..."
-        else
-            mv $HOME/.config/$file $oldconfig/
-            echo "Creating symlink to $file in $HOME/.config directory."
-            ln -s $config/$file $HOME/.config/$file
-        fi
-    done
-
-    echo ""
-    echo "#########################################"
-
-    # check if backup dirs are empty
-    if [ ! "$(ls -A $olddir)" ]; then
-        echo "$olddir is empty, removing..."
-        rmdir $olddir
-    fi
-
-    if [ ! "$(ls -A $oldconfig)" ]; then
-        echo "$oldconfig is empty, removing..."
-        rmdir $oldconfig
-    fi
-}
-##### End basic ################################################################
-
-##### HELP #####################################################################
-function _help
-{
-    echo "$usage"
-    echo "HELP Text"
-}
-##### End HELP #################################################################
-
-########## End Functions #######################################################
-
 ########## Variables ###########################################################
-usage="usage: \"$0 -[ahir]\""
+usage="usage: \"$0 -[hir]\""
 
-# dotfiles directory
+isRoot=0
+interactive=0
+
+# directories
 dir=$HOME/.dotfiles
 config=$HOME/.dotfiles/config
 
-# old dotfiles backup directory
+# backup directories
 olddir=$HOME/.dotfiles_old
 oldconfig=$HOME/.config_old
 
@@ -110,17 +40,64 @@ files="bashrc\
 config_files="redshift.conf"
 ########## End Variables #######################################################
 
-########## getopts #############################################################
-while getopts ahir: FLAG; do
+########## Functions ###########################################################
+##### _help ####################################################################
+function _help
+{
+    echo "$usage"
+    echo "HELP Text"
+}
+##### End _help ################################################################
+
+##### makeSymLinks #############################################################
+# $1 destination, $2 source, $3 destination backup,
+# $4 use dot (0|1), $5 files (can be a list)
+function makeSymLinks
+{
+    dest=$1
+    shift
+    src=$1
+    shift
+    backup=$1
+    shift
+    useDot=$1
+    shift
+    files=$*
+
+    echo "Moving any existing dotfiles from $dest to $backup"
+    for file in $files; do
+        if [ $useDot -eq 1 ]; then
+            if [ "$(readlink $dest/.$file)" = $src/$file ]; then
+                echo "$file is already linked here, skipping..."
+            else
+                mv $dest/.$file $backup/
+                echo "Creating symlink to $file in $dest"
+                ln -s $src/$file $dest/.$file
+            fi
+        else
+            if [ "$(readlink $dest/$file)" = $src/$file ]; then
+                echo "$file is already linked here, skipping..."
+            else
+                mv $dest/$file $backup/
+                echo "Creating symlink to $file in $dest"
+                ln -s $src/$file $dest/$file
+            fi
+        fi
+    done
+}
+##### End makeSymLinks #########################################################
+########## End Functions #######################################################
+
+########## Code ################################################################
+##### getopts ##################################################################
+while getopts hir: FLAG; do
     case $FLAG in
-        a) #all
-            basic
-            ;;
         h) #help
             _help
+            exit
             ;;
         i) #interactive
-            #stuff
+            interactive=1
             ;;
         r) #root
             if [[ $EUID -eq 0 ]]; then
@@ -138,5 +115,45 @@ while getopts ahir: FLAG; do
 done
 
 shift $((OPTIND-1)) #Moves getopts to the next argument
-########## End getopts #########################################################
+##### End getopts ##############################################################
 
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in $HOME"
+mkdir -p $olddir
+
+# create config_old in homedir
+echo "Creating $oldconfig for backup of any existing files in $HOME/.config"
+mkdir -p $oldconfig
+
+# change to the dotfiles directory
+echo ""
+echo "#########################################"
+echo "Changing to the $dir directory ..."
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the $HOME/dotfiles directory specified in $files
+makeSymLinks $HOME $dir $olddir 1 $files
+
+# change to config directory
+echo ""
+echo "#########################################"
+echo "Changing to the $config directory ..."
+cd $config
+
+# move existing and make symlinks
+makeSymLinks $HOME/.config $config $oldconfig 0 $config_files
+
+echo ""
+echo "#########################################"
+
+# check if backup dirs are empty
+if [ ! "$(ls -A $olddir)" ]; then
+    echo "$olddir is empty, removing..."
+    rmdir $olddir
+fi
+
+if [ ! "$(ls -A $oldconfig)" ]; then
+    echo "$oldconfig is empty, removing..."
+    rmdir $oldconfig
+fi
+########## End Code ############################################################
