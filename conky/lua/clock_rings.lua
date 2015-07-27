82785e294cc212d8ab8b54e9f228521502728beb
Bradford Smith <bradford.smith94@gmail.com>
2015-07-27 15:38:50 -0400

    Conky: removed lua clock hands option.
    Cleaned up some of the comments in the clock_rings.lua script and removed the
    code that allows the drawing of the clock hands.

--- conky/lua/clock_rings.lua
+++ conky/lua/clock_rings.lua
@@ -1,50 +1,27 @@
 --[[
 Clock Rings by Linux Mint (2011) reEdited by despot77
 
-This script draws percentage meters as rings, and also draws clock hands if you want! It is fully customisable; all options are described in the script. This script is based off a combination of my clock.lua script and my rings.lua script.
-
-IMPORTANT: if you are using the 'cpu' function, it will cause a segmentation fault if it tries to draw a ring straight away. The if statement on line 145 uses a delay to make sure that this doesn't happen. It calculates the length of the delay by the number of updates since Conky started. Generally, a value of 5s is long enough, so if you update Conky every 1s, use update_num>5 in that if statement (the default). If you only update Conky every 2s, you should change it to update_num>3; conversely if you update Conky every 0.5s, you should use update_num>10. ALSO, if you change your Conky, is it best to use "killall conky; conky" to update it, otherwise the update_num will not be reset and you will get an error.
-
 To call this script in Conky, use the following (assuming that you save this script to ~/scripts/rings.lua):
     lua_load ~/scripts/clock_rings.lua
     lua_draw_hook_pre clock_rings
 
-Changelog:
-+ v1.0 -- Original release (30.09.2009)
-   v1.1p -- Jpope edit londonali1010 (05.10.2009)
-*v 2011mint -- reEdit despot77 (18.02.2011)
-
 Edited by Bradford Smith
-updated: 4/20/2015
+updated: 7/27/2015
 ]]
 
 settings_table = {
     {
-        -- Edit this table to customise your rings.
-        -- You can create more rings simply by adding more elements to settings_table.
-        -- "name" is the type of stat to display; you can choose from 'cpu', 'memperc', 'fs_used_perc', 'battery_used_perc'.
         name='time',
-        -- "arg" is the argument to the stat type, e.g. if in Conky you would write ${cpu cpu0}, 'cpu0' would be the argument. If you would not use an argument in the Conky variable, use ''.
         arg='%I.%M', --Hour
-        -- "max" is the maximum value of the ring. If the Conky variable outputs a percentage, use 100.
         max=12,
-        -- "bg_color" is the color of the base ring.
         bg_color=0x000000,
-        -- "bg_alpha" is the alpha value of the base ring.
         bg_alpha=0.3,
-        -- "fg_color" is the color of the indicator part of the ring.
         fg_color=0x1793D0,
-        -- "fg_alpha" is the alpha value of the indicator part of the ring.
         fg_alpha=0.6,
-        -- "x" and "y" are the x and y coordinates of the centre of the ring, relative to the top left corner of the Conky window.
         x=65, y=65,
-        -- "radius" is the radius of the ring.
         radius=20,
-        -- "thickness" is the thickness of the ring, centred around the radius.
         thickness=20,
-        -- "start_angle" is the starting angle of the ring, in degrees, clockwise from top. Value can be either positive or negative.
         start_angle=0,
-        -- "end_angle" is the ending angle of the ring, in degrees, clockwise from top. Value can be either positive or negative, but must be larger than start_angle.
         end_angle=360
     },
     {
@@ -244,53 +221,6 @@ function draw_ring(cr,t,pt)
     cairo_stroke(cr)
 end
 
-function draw_clock_hands(cr,xc,yc)
-    local secs,mins,hours,secs_arc,mins_arc,hours_arc
-    local xh,yh,xm,ym,xs,ys
-
-    secs=os.date("%S")
-    mins=os.date("%M")
-    hours=os.date("%I")
-
-    secs_arc=(2*math.pi/60)*secs
-    mins_arc=(2*math.pi/60)*mins+secs_arc/60
-    hours_arc=(2*math.pi/12)*hours+mins_arc/12
-
-    -- Draw hour hand
-
-    xh=xc+0.7*clock_r*math.sin(hours_arc)
-    yh=yc-0.7*clock_r*math.cos(hours_arc)
-    cairo_move_to(cr,xc,yc)
-    cairo_line_to(cr,xh,yh)
-
-    cairo_set_line_cap(cr,CAIRO_LINE_CAP_ROUND)
-    cairo_set_line_width(cr,3)
-    cairo_set_source_rgba(cr,1.0,1.0,1.0,1.0)
-    cairo_stroke(cr)
-
-    -- Draw minute hand
-
-    xm=xc+clock_r*math.sin(mins_arc)
-    ym=yc-clock_r*math.cos(mins_arc)
-    cairo_move_to(cr,xc,yc)
-    cairo_line_to(cr,xm,ym)
-
-    cairo_set_line_width(cr,2)
-    cairo_stroke(cr)
-
-    -- Draw seconds hand
-
-    if show_seconds then
-        xs=xc+clock_r*math.sin(secs_arc)
-        ys=yc-clock_r*math.cos(secs_arc)
-        cairo_move_to(cr,xc,yc)
-        cairo_line_to(cr,xs,ys)
-
-        cairo_set_line_width(cr,1)
-        cairo_stroke(cr)
-    end
-end
-
 function conky_clock_rings()
     local function setup_rings(cr,pt)
         local str=''
@@ -320,6 +250,4 @@ function conky_clock_rings()
             setup_rings(cr,settings_table[i])
         end
     end
-
-    --draw_clock_hands(cr,clock_x,clock_y)
 end
