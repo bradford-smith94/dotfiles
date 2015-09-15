790d5b3d6e0cfb60428cf5992afb0d30050f1ec2
Bradford Smith <bradford.smith94@gmail.com>
2015-09-14 22:58:41 -0400

    reflect_mirrors.sh now removes mirrorlist.pacnew
    When pacman updates the mirrorlist it creates `mirrorlist.pacnew`, now running
    `reflect_mirrors.sh` will also remove `mirrorlist.pacnew`. Also added some
    documentation to the script.

--- scripts/reflect_mirrors.sh
+++ scripts/reflect_mirrors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ##############
-# Run reflector on ${dir}mirrorlist to keep them up to date
+# Run reflector on /etc/mirrorlist to keep them up to date
 # Bradford Smith
-# updated: 5/11/2015
-####################
+# updated: 09/14/2015
+######################
 
 # check to make sure we are running as root
 if [[ $EUID -ne 0 ]]; then
@@ -14,7 +14,17 @@ fi
 # set mirrorlist dir
 dir=/etc/pacman.d
 
+# remove mirrorlist.pacnew if it exists
+if [ -f $dir/mirrorlist.pacnew ]; then
+    rm $dir/mirrorlist.pacnew
+fi
+
+# get new mirrorlist from archlinux.org
 wget -O $dir/mirrorlist.new https://www.archlinux.org/mirrorlist/all/
+
+# backup old mirrorlist and replace with new one
 cp $dir/mirrorlist $dir/mirrorlist.backup
 mv $dir/mirrorlist.new $dir/mirrorlist
+
+# run reflector to rank the mirrors
 reflector --verbose -c 'United States' -l 200 --sort rate --save $dir/mirrorlist
