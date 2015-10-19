56d28e3b8137a35050051651db8f4689a600284e
Bradford Smith <bradford.smith94@gmail.com>
2015-10-19 14:53:03 -0400

    Git config updates
    Trying to get a script working that will update the value of push.default. This
    script currently doesn't work.

---
+++ scripts/hasGitPushSimple.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Bradford Smith
+# hasGitPushSimple.sh
+# updated: 10/19/2015
+#####################
+# The option "git push.default = simple" was introduced in version 1.7.11
+
+
+GITVERSION="$(git --version | awk '{print $3}')"
+MAJOR="$(echo "${GITVERSION}" | awk -F'.' '{print $1}')"
+MINOR="$(echo "${GITVERSION}" | awk -F'.' '{print $2}')"
+UPDATE="$(echo "${GITVERSION}" | awk -F'.' '{print $3}')"
+
+HASGITPUSHSIMPLE=0
+
+if [ ${MAJOR} -le 1 ]; then
+    if [ ${MINOR} -le 7 ]; then
+        if [ ${UPDATE} -le 10]; then
+            HASGITPUSHSIMPLE=0
+        else
+            HASGITPUSHSIMPLE=1
+        fi
+    else
+        HASGITPUSHSIMPLE=1
+    fi
+else
+    HASGITPUSHSIMPLE=1
+fi
+
+if [ ${HASGITPUSHSIMPLE} ]; then
+    sed "s/push_default/simple/g" ~/.gitconfig > ~/.gitconfig
+else
+    sed "s/push_default/upstream/g" ~/.gitconfig > ~/.gitconfig
+fi
