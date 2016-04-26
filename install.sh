a0bd6619a68cabbab9b8b2b195d83100ca8b3bd9
Bradford Smith <bradford.smith94@gmail.com>
2016-04-25 23:55:25 -0400

    xprofile: added xprofile to repo
    The configuration for the Caps Lock key is done with `setxkbmap` and `xcape` in
    the `~/.xprofile` file.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #{{{############################################################################
 # Bradford Smith
 # install.sh
-# updated: 04/18/2016
+# updated: 04/25/2016
 #
 # This script can be run to install my dotfiles.
 #
@@ -28,6 +28,7 @@ oldconfig=$HOME/.config_old
 # list of files/folders to symlink in homedir
 files="tmux.conf\
  Xresources\
+ xprofile\
  w3m"
 
 # groups that have dependent files
