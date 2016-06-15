bf469a5400c85c65b601727b43a37f8bf6d409d7
Bradford Smith <bradford.smith94@gmail.com>
2016-06-14 23:45:27 -0400

    bin: updated fixPerms.sh to take in a directory
    The `fixPerms.sh` script now optionally takes in a directory in which to fix
    permissions.

--- bin/fixPerms.sh
+++ bin/fixPerms.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # fixPerms.sh
 # Recursively chmods directories to 755 and their files to 644
+# takes in:
+#   $1: directory to work in [optional]
 
+# if given an argument assume it's a directory
+[ $# -eq 1 ] && { echo "entering: $1"; cd "$1"; }
+
+# set all directories to 755 and all files to 644
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
+
+# if we were given a directory go back to where we were before
+[ $# -eq 1 ] && { echo "leaving: $1"; cd -; }
