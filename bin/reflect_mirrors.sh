4f6de7919114ff447a9bd52505fe715fcb9ac08f
Bradford Smith <bradford.smith94@gmail.com>
2016-06-23 12:33:14 -0400

    Update reflector scripts
    This is an update for `bin/reflect_mirrors.sh` and my pacman hook for updating
    the mirrorlist `etc/pacman.d/hooks/mirrorlist.hook` and
    `etc/pacman.d/hooks/mirrorlist.sh`.
    
    Both scripts do exactly the same thing now, move the current mirrorlist to a
    backup then download a new one from archlinux.org before running reflector to
    filter only USA mirrors and rank them by fastest 200.

--- bin/reflect_mirrors.sh
+++ bin/reflect_mirrors.sh
@@ -2,13 +2,13 @@
 ##############
 # Run reflector on /etc/pacman.d/mirrorlist to keep them up to date
 # Bradford Smith
-# updated: 09/14/2015
+# updated: 06/23/2016
 ######################
 
 # check to make sure we are running as root
 if [[ $EUID -ne 0 ]]; then
-	echo "Please run this script as root"
-	exit 1
+    echo "Please run this script as root"
+    exit 1
 fi
 
 # set mirrorlist dir
@@ -19,12 +19,25 @@ if [ -f $dir/mirrorlist.pacnew ]; then
     rm $dir/mirrorlist.pacnew
 fi
 
+# backup old mirrorlist
+mv $dir/mirrorlist $dir/mirrorlist.backup
+
 # get new mirrorlist from archlinux.org
-wget -O $dir/mirrorlist.new https://www.archlinux.org/mirrorlist/all/
+if wget -q -O $dir/mirrorlist https://www.archlinux.org/mirrorlist/all/ ; then
 
-# backup old mirrorlist and replace with new one
-cp $dir/mirrorlist $dir/mirrorlist.backup
-mv $dir/mirrorlist.new $dir/mirrorlist
+    # run reflector to sort mirrors by fastest 200 USA mirrors
+    if reflector -c 'United States' -l 200 --sort rate --save $dir/mirrorlist ; then
+        echo "Reflector updated mirrorlist successfully"
+    else
+        echo "Reflector failed, replacing old mirrorlist" 1>&2
+        err=1
+    fi
+else
+    echo "Wget failed, replacing old mirrorlist" 1>&2
+    err=1
+fi
 
-# run reflector to rank the mirrors
-reflector --verbose -c 'United States' -l 200 --sort rate --save $dir/mirrorlist
+# on error copy back the backed-up mirrorlist
+if [[ $err -eq 1 ]]; then
+    cp $dir/mirrorlist.backup $dir/mirrorlist
+fi
--- etc/pacman.d/hooks/mirrorlist.sh
+++ etc/pacman.d/hooks/mirrorlist.sh
@@ -1,10 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 # Script for the pacman hook that updates the mirrorlist
 # This file should be placed at `/etc/pacman.d/hooks/mirrorlist.sh`
 
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-reflector -c 'United States' -l 200 --sort rate --save /etc/pacman.d/mirrorlist
+# mirrorlist directory
+dir=/etc/pacman.d
 
+# remove mirrorlist.pacnew if it exists
 if [ -f /etc/pacman.d/mirrorlist.pacnew ]; then
     rm /etc/pacman.d/mirrorlist.pacnew
+fi
+
+# backup old mirrorlist
+mv $dir/mirrorlist $dir/mirrorlist.backup
+
+# get new mirrorlist from archlinux.org
+if wget -q -O $dir/mirrorlist https://www.archlinux.org/mirrorlist/all/ ; then
+
+    # run reflector to sort mirrors by fastest 200 USA mirrors
+    if reflector -c 'United States' -l 200 --sort rate --save $dir/mirrorlist ; then
+        echo "Reflector updated mirrorlist successfully"
+    else
+        echo "Reflector failed, replacing old mirrorlist" 1>&2
+        err=1
+    fi
+else
+    echo "Wget failed, replacing old mirrorlist" 1>&2
+    err=1
+fi
+
+# on error copy back the backed-up mirrorlist
+if [[ $err -eq 1 ]]; then
+    cp $dir/mirrorlist.backup $dir/mirrorlist
 fi
