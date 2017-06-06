11f30140245cfca6facd3ed4a4f941bb5fa76a01
Bradford Smith <bradford.smith94@gmail.com>
2017-06-06 12:36:38 -0400

    install.sh: Fix shellcheck warnings
    Shellcheck warned about lots of quoting issues, some unused variables
    and options for 'read'. This adds the missing quotes, comments the
    unused color variables and adds the suggested option for 'read' in order
    to satisfy Shellcheck.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #{{{############################################################################
 # Bradford Smith
 # install.sh
-# updated: 03/22/2017
+# updated: 06/06/2017
 #
 # This script can be run to install my dotfiles.
 #
@@ -25,26 +25,26 @@ interactive=$TRUE
 force=$FALSE
 
 # colors
-BLACK='\033[0;30m'
+#BLACK='\033[0;30m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
+#BLUE='\033[0;34m'
+#PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
-L_GREY='\033[0;37m'
-D_GREY='\033[1;30m'
-L_RED='\033[1;31m'
-L_GREEN='\033[1;32m'
-L_YELLOW='\033[1;33m'
-L_BLUE='\033[1;34m'
-L_PURPLE='\033[1;35m'
-L_CYAN='\033[1;36m'
-WHITE='\033[1;37m'
+#L_GREY='\033[0;37m'
+#D_GREY='\033[1;30m'
+#L_RED='\033[1;31m'
+#L_GREEN='\033[1;32m'
+#L_YELLOW='\033[1;33m'
+#L_BLUE='\033[1;34m'
+#L_PURPLE='\033[1;35m'
+#L_CYAN='\033[1;36m'
+#WHITE='\033[1;37m'
 NC='\033[0m'
 
 # project directories
-dir=$(dirname $(readlink -f $0))
+dir=$(dirname "$(readlink -f "$0")")
 config=$dir/config
 
 # target directories
@@ -133,7 +133,7 @@ EndHelpText
 function printSep
 {
     if [ $# -gt 0 ]; then
-        printf '\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' $1
+        printf '\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "$1"
     else
         printf '\n%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
     fi
@@ -162,8 +162,8 @@ function promptYN
 
     echo -n "$question? $prompt "
 
-    read response
-    response=$(echo $response | tr '[:upper:]' '[:lower:]')
+    read -r response
+    response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
 
     while [[ $response != "n" && \
         $response != "no" && \
@@ -173,7 +173,7 @@ function promptYN
             return $default
         fi
         echo -ne "${RED}Type 'y/yes' or 'n/no': ${NC}"
-        read response
+        read -r response
     done
 
     if [[ $response == "n" || \
@@ -200,7 +200,7 @@ function makeSymLink
     backup=$4
     useDot=$5
 
-    if grep -wq $file $installed_files ; then
+    if grep -wq "$file" "$installed_files" ; then
         echo -e "${YELLOW}$file was already done this session, skipping...${NC}"
         return
     fi
@@ -212,23 +212,23 @@ function makeSymLink
     fi # end if interactive
 
     target=$dest/$file
-    if [ $useDot -eq $TRUE ]; then
+    if [ "$useDot" -eq $TRUE ]; then
         target=$dest/.$file
     fi # end if useDot
 
-    echo $file >> $installed_files
-    if [ "$(readlink $target 2> /dev/null)" = $src/$file ]; then
+    echo "$file" >> "$installed_files"
+    if [ "$(readlink "$target" 2> /dev/null)" = "$src/$file" ]; then
         if [ $force -eq $TRUE ]; then
             echo -e "${GREEN}Creating symlink to $file in $dest${NC}"
-            unlink $target
-            ln -s $src/$file $target
+            unlink "$target"
+            ln -s "$src/$file" "$target"
         else
             echo -e "${CYAN}$file is already linked here, skipping...${NC}"
         fi
     else
-        mv $target $backup/ 2> /dev/null
+        mv "$target" "$backup/" 2> /dev/null
         echo -e "${GREEN}Creating symlink to $file in $dest${NC}"
-        ln -s $src/$file $target
+        ln -s "$src/$file" "$target"
     fi
 }
 #}}} End makeSymLinks ----------------------------------------------------------
@@ -263,15 +263,15 @@ shift $((OPTIND-1)) #Moves getopts to the next argument
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in $HOME"
-mkdir -p $olddir
+mkdir -p "$olddir"
 
 # create config_old in homedir
 echo "Creating $oldconfig for backup of any existing files in $HOME/.config"
-mkdir -p $oldconfig
+mkdir -p "$oldconfig"
 
 # create config_target
 echo "Creating $config_target in case it doesn't exist yet"
-mkdir -p $config_target
+mkdir -p "$config_target"
 
 #{{{ install dotfiles ----------------------------------------------------------
 printSep "="
@@ -279,7 +279,7 @@ printSep "="
 # install non-dependent files from the 'files' list
 for ((i = 0; i < ${#files[@]}; i++)); do
     arr=(${files[$i]})
-    makeSymLink ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]}
+    makeSymLink "${arr[0]}" "${arr[1]}" "${arr[2]}" "${arr[3]}" "${arr[4]}"
 done
 
 # install Vim group
@@ -289,7 +289,7 @@ if [ $interactive -eq $FALSE ] || promptYN "Link Vim Configuration group" "y"; t
 
     for ((i = 0; i < ${#vim[@]}; i++)); do
         arr=(${vim[$i]})
-        makeSymLink ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]}
+        makeSymLink "${arr[0]}" "${arr[1]}" "${arr[2]}" "${arr[3]}" "${arr[4]}"
     done
 
     interactive=$saved_interactive
@@ -302,7 +302,7 @@ if [ $interactive -eq $FALSE ] || promptYN "Link Git Configuration group" "y"; t
 
     for ((i = 0; i < ${#git[@]}; i++)); do
         arr=(${git[$i]})
-        makeSymLink ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]}
+        makeSymLink "${arr[0]}" "${arr[1]}" "${arr[2]}" "${arr[3]}" "${arr[4]}"
     done
 
     interactive=$saved_interactive
@@ -315,7 +315,7 @@ if [ $interactive -eq $FALSE ] || promptYN "Link Bash Configuration group" "y"; 
 
     for ((i = 0; i < ${#bash[@]}; i++)); do
         arr=(${bash[$i]})
-        makeSymLink ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]}
+        makeSymLink "${arr[0]}" "${arr[1]}" "${arr[2]}" "${arr[3]}" "${arr[4]}"
     done
 
     interactive=$saved_interactive
@@ -328,7 +328,7 @@ if [ $interactive -eq $FALSE ] || promptYN "Link Zsh Configuration group" "y"; t
 
     for ((i = 0; i < ${#zsh[@]}; i++)); do
         arr=(${zsh[$i]})
-        makeSymLink ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]}
+        makeSymLink "${arr[0]}" "${arr[1]}" "${arr[2]}" "${arr[3]}" "${arr[4]}"
     done
 
     interactive=$saved_interactive
@@ -341,7 +341,7 @@ if [ $interactive -eq $FALSE ] || promptYN "Link Bspwm Configuration group" "y";
 
     for ((i = 0; i < ${#bspwm[@]}; i++)); do
         arr=(${bspwm[$i]})
-        makeSymLink ${arr[0]} ${arr[1]} ${arr[2]} ${arr[3]} ${arr[4]}
+        makeSymLink "${arr[0]}" "${arr[1]}" "${arr[2]}" "${arr[3]}" "${arr[4]}"
     done
 
     interactive=$saved_interactive
@@ -351,19 +351,19 @@ fi
 printSep "="
 
 # check if backup dirs are empty and clean if they are
-if [ ! "$(ls -A $olddir)" ]; then
+if [ ! "$(ls -A "$olddir")" ]; then
     echo "$olddir is empty, removing..."
-    rmdir $olddir
+    rmdir "$olddir"
 fi
 
-if [ ! "$(ls -A $oldconfig)" ]; then
+if [ ! "$(ls -A "$oldconfig")" ]; then
     echo "$oldconfig is empty, removing..."
-    rmdir $oldconfig
+    rmdir "$oldconfig"
 fi
 
-if [ ! "$(ls -A $config_target)" ]; then
+if [ ! "$(ls -A "$config_target")" ]; then
     echo "$config_target is empty, we must have created it, removing..."
-    rmdir $config_target
+    rmdir "$config_target"
 fi
 
 # check for broken symlinks
@@ -374,7 +374,7 @@ if [ $remove_broken_links -eq $TRUE ] || \
     [ $interactive -eq $TRUE ] && promptYN "Check for broken symlinks" ""; then
 
     #use sed to quote files in case they contain spaces
-    broken=$(find $HOME -type l ! -exec test -e {} \; -print | sed -e 's/^/"/g' -e 's/$/"/g')
+    broken=$(find "$HOME" -type l ! -exec test -e {} \; -print | sed -e 's/^/"/g' -e 's/$/"/g')
 
     if [ -n "$broken" ]; then
         echo -e "${RED}Detected broken symlinks${NC}"
@@ -386,7 +386,7 @@ if [ $remove_broken_links -eq $TRUE ] || \
         for ((i = 0; i < ${#arr[@]}; i++)); do
             if promptYN "Remove broken link: ${arr[$i]}" "y"; then
                 #unquote the file so we can remove it properly
-                file=$(echo ${arr[$i]} | sed -e 's/^"//g' -e 's/"$//g')
+                file=$(echo "${arr[$i]}" | sed -e 's/^"//g' -e 's/"$//g')
                 unlink "$file"
             fi
         done
