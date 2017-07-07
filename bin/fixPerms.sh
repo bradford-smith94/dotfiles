e08c2764542a453091d9a05d522384ccaf5df7a4
Bradford Smith <bradford.smith94@gmail.com>
2017-07-07 16:45:40 -0400

    bin: Make fixPerms.sh use directory stack
    This updates fixPerms.sh to use the directory stack instead of plain
    `cd`, it shouldn't change how it functions and will allow additional
    directory traversal within the script if ever necessary.

--- bin/fixPerms.sh
+++ bin/fixPerms.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-# fixPerms.sh
+# Bradford Smith
+# ~/bin/fixPerms.sh
+# 07/07/2017
+###################
+
 # Recursively chmods directories to 755 and their files to 644
 # takes in:
 #   $1: directory to work in [optional]
 
 # if given an argument assume it's a directory
-[ $# -eq 1 ] && { echo "entering: $1"; cd "$1"; }
+[ $# -eq 1 ] && { echo "entering: $1"; pushd "$1"; }
 
 # set all directories to 755 and all files to 644
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
 
 # if we were given a directory go back to where we were before
-[ $# -eq 1 ] && { echo "leaving: $1"; cd -; }
+[ $# -eq 1 ] && { echo "leaving: $1"; popd; }
