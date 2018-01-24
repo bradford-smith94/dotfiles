bradford-smith94/dotfiles

commit c217dcaa36db8bda1916a4e194c1b6c75643af85

shell: Add vim helper to format man-like input
In the $MANPAGER variable vim will call a new function that I just added to do
some basic formatting (`col -b`) if the filetype isn't already set to 'man'.
This makes things that aren't 'man' that interact with the system pager like
'man' does work. For example, some python utilities have a help subcommand that
prints a manpage using the system pager, however they do not set $MAN_PN like
'man' does so they miss my initial manpage detection.

This will set the wrong filetype if something uses the pager for input that
isn't man-like in syntax, right now I consider this to be a minor drawback as I
don't know of any examples of this in my current workflow.

vim/autoload/manpage.vim
@@ -1,17 +1,27 @@
 " Bradford Smith
 " ~/.vim/autoload/manpage.vim
-" 09/12/2017
-" Helper function for saving manpage as a tmpfile
+" 01/24/2018
+" Manpage helper functions
 
+" Format the current buffer as a manpage if it isn't already
+function! manpage#Prettify()
+    if &filetype == 'man'
+        finish
+    endif
+    silent execute '%! col -b'
+    setlocal filetype=man
+endfunction
+
+" Helper function for saving manpage as a tmpfile
 function! manpage#SaveStdinManpage()
     let s:tmp_dir = system("mktemp -d 2>/dev/null || mktemp -d -t 'vim-tmp'")
-    execute "chdir " . s:tmp_dir
-    silent execute "%! col -b"
+    execute 'chdir ' . s:tmp_dir
+    silent execute '%! col -b'
     file $MAN_PN
     write
     setlocal filetype=man
     augroup tmp_manpage_delete
         autocmd!
-        autocmd VimLeavePre * execute "!rm -r " . s:tmp_dir
+        autocmd VimLeavePre * execute '!rm -r ' . s:tmp_dir
     augroup END
 endfunction

shell_env
@@ -1,6 +1,6 @@
 # Bradford Smith
 # ~/.shell_env
-# updated: 01/23/2018
+# updated: 01/24/2018
 #####################
 
 # turn off flow control (ctrl-q and ctrl-s)
@@ -51,7 +51,7 @@ export PATH=$PATH
 # use vim as the default editor and man pager
 if command -v vim >/dev/null 2>&1; then
     export EDITOR="vim"
-    export MANPAGER="vim -"
+    export MANPAGER="vim -c 'call manpage#Prettify()' -"
 fi
 
 # enable xterm transparency with transset-df

commit 827f6fe055c611651571c2b52aab0f0437ec400f

vim: Fix manpage#Prettify() finish error
Fixes error introduced in: c217dcaa36db8bda1916a4e194c1b6c75643af85

When viewing a regular manpage the manpage#Prettify() function should do
nothing. It produced an error that 'finish' was used outside of a
sourced file. To fix this I changed the conditional to the opposite and
wrapped the function content.

vim/autoload/manpage.vim
@@ -5,11 +5,10 @@
 
 " Format the current buffer as a manpage if it isn't already
 function! manpage#Prettify()
-    if &filetype == 'man'
-        finish
+    if &filetype !=# 'man'
+        silent execute '%! col -b'
+        setlocal filetype=man
     endif
-    silent execute '%! col -b'
-    setlocal filetype=man
 endfunction
 
 " Helper function for saving manpage as a tmpfile